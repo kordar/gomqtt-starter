49ceca7af541cbcd97fc0a89d854bcd521f6307c
perpel <572821520@qq.com>
2024-10-13 16:09:13 +0800

    m

--- starter.go
+++ starter.go
@@ -3,7 +3,7 @@ package gomqtt_starter
 import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	goframeworkmqtt "github.com/kordar/goframework-mqtt"
-	"github.com/kordar/gologger"
+	logger "github.com/kordar/gologger"
 	"github.com/spf13/cast"
 )
 
@@ -18,10 +18,10 @@ var (
 
 type MqttModule struct {
 	name string
-	load func(name string, value map[string]string)
+	load func(moduleName string, itemId string, item map[string]string)
 }
 
-func NewMqttModule(name string, load func(moduleName string, item map[string]string)) *MqttModule {
+func NewMqttModule(name string, load func(moduleName string, itemId string, item map[string]string)) *MqttModule {
 	return &MqttModule{name, load}
 }
 
@@ -29,18 +29,37 @@ func (m MqttModule) Name() string {
 	return m.name
 }
 
+func (m MqttModule) _load(id string, cfg map[string]string) {
+	if id == "" {
+		logger.Fatalf("[%s] the attribute id cannot be empty.", m.Name())
+		return
+	}
+
+	err := goframeworkmqtt.AddMqttInstanceArgs(id, cfg, CredentialsProvider, DefaultPublishHandler, OnConnect, OnConnectionLost, OnReconnecting, OnConnectAttempt)
+	if err != nil {
+		logger.Errorf("[gomqtt-starter] 初始化mqtt异常，err=%v", err)
+		return
+	}
+
+	if m.load != nil {
+		m.load(m.name, id, cfg)
+		logger.Debugf("[%s] triggering custom loader completion", m.Name())
+	}
+
+	logger.Infof("[%s] loading module '%s' successfully", m.Name(), id)
+}
+
 func (m MqttModule) Load(value interface{}) {
+
 	items := cast.ToStringMap(value)
-	for key, val := range items {
-		section := cast.ToStringMapString(val)
-		err := goframeworkmqtt.AddMqttInstanceArgs(key, section, CredentialsProvider, DefaultPublishHandler, OnConnect, OnConnectionLost, OnReconnecting, OnConnectAttempt)
-		if err != nil {
-			logger.Errorf("[gomqtt-starter] 初始化mqtt异常，err=%v", err)
-			continue
-		}
-		if m.load != nil {
-			m.load(m.Name(), section)
-		}
+	if items["id"] != nil {
+		id := cast.ToString(items["id"])
+		m._load(id, cast.ToStringMapString(value))
+		return
+	}
+
+	for key, item := range items {
+		m._load(key, cast.ToStringMapString(item))
 	}
 
 }
